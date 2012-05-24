4d63d4631ef40b99b01959d514629c5b131e1045
Mark Wadham <m@rkw.me>
2012-05-24 15:36:33 +0100

    removed the delete button from the view template as this is in the edit bar now.

--- views/default/view.php
+++ views/default/view.php
@@ -17,9 +17,6 @@
 <div class="form_button">
 	<img class="loader" style="display: none;" src="/img/ajax-loader.gif" alt="loading..." />
 	<button type="submit" class="classy blue venti" id="et_print" name="print"><span class="button-span button-span-blue">Print</span></button>
-	<?php if ($this->event->canDelete()) {?>
-		<button type="submit" class="classy red venti" id="et_delete" name="delete"><span class="button-span button-span-red">Delete</span></button>
-	<?php }?>
 </div>
 
 <?php $this->footer() ?>
