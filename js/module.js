ac9612db961fd4526725483253a156f3d300d2c1
Mark Wadham <m@rkw.me>
2012-09-17 08:47:54 +0100

    bugfix for event deletion bug.

--- js/module.js
+++ js/module.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	$('#et_deleteevent').unbind('click').click(function() {
 		if (!$(this).hasClass('inactive')) {
 			disableButtons();
-			$('#deleteForm').submit();
+			$('#et_deleteevent').click();
 		}
 		return false;
 	});
