2465b42d46ef09945804e342d3fe2ed637f7b258
Mark <m@rkw.me>
2013-02-28 11:45:35 +0000

    [OE-2714] updated to use the new handleButton method

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,65 +1,47 @@
 $(document).ready(function() {
-	$('#et_save_print').unbind('click').click(function() {
-		if (!$(this).hasClass('inactive')) {
-			disableButtons();
-			$('#Element_OphDrPrescription_Details_draft').val(0);
-			return true;
-		}
-		return false;
+	handleButton($('#et_save_print'),function() {
+		$('#Element_OphDrPrescription_Details_draft').val(0);
 	});
 
-	$('#et_print').unbind('click').click(function() {
+	handleButton($('#et_print'),function(e) {
 		do_print_prescription();
-		return false;
+		e.preventDefault();
 	});
 
-	$('#et_deleteevent').unbind('click').click(function() {
-		if (!$(this).hasClass('inactive')) {
-			disableButtons();
-			return true;
+	handleButton($('#et_deleteevent'));
+
+	handleButton($('#et_canceldelete'),function(e) {
+		if (m = window.location.href.match(/\/delete\/[0-9]+/)) {
+			window.location.href = window.location.href.replace('/delete/','/view/');
+		} else {
+			window.location.href = baseUrl+'/patient/episodes/'+et_patient_id;
 		}
-		return false;
+		e.preventDefault();
 	});
 
-	$('#et_canceldelete').unbind('click').click(function() {
-		if (!$(this).hasClass('inactive')) {
-			disableButtons();
+	handleButton($('#et_cancel'),function(e) {
+		$('#dialog-confirm-cancel').dialog({
+			resizable: false,
+			//height: 140,
+			modal: true,
+			buttons: {
+				"Yes, cancel": function() {
+					$(this).dialog('close');
 
-			if (m = window.location.href.match(/\/delete\/[0-9]+/)) {
-				window.location.href = window.location.href.replace('/delete/','/view/');
-			} else {
-				window.location.href = baseUrl+'/patient/episodes/'+et_patient_id;
-			}
-		}
-		return false;
-	});
+					disableButtons();
 
-	$('#et_cancel').unbind('click').click(function() {
-		if (!$(this).hasClass('inactive')) {
-			$('#dialog-confirm-cancel').dialog({
-				resizable: false,
-				//height: 140,
-				modal: true,
-				buttons: {
-					"Yes, cancel": function() {
-						$(this).dialog('close');
-
-						disableButtons();
-
-						if (m = window.location.href.match(/\/update\/[0-9]+/)) {
-							window.location.href = window.location.href.replace('/update/','/view/');
-						} else {
-							window.location.href = baseUrl+'/patient/episodes/'+et_patient_id;
-						}
-					},
-					"No, go back": function() {
-						$(this).dialog('close');
-						return false;
+					if (m = window.location.href.match(/\/update\/[0-9]+/)) {
+						window.location.href = window.location.href.replace('/update/','/view/');
+					} else {
+						window.location.href = baseUrl+'/patient/episodes/'+et_patient_id;
 					}
+				},
+				"No, go back": function() {
+					$(this).dialog('close');
 				}
-			});
-		}
-		return false;
+			}
+		});
+		e.preventDefault();
 	});
 
 });
@@ -68,4 +50,3 @@ function do_print_prescription() {
 	var m = window.location.href.match(/\/view\/([0-9]+)/);
 	printPDF(baseUrl+'/OphDrPrescription/default/print/' + m[1],{});
 }
-
