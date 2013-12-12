376c50617bff1cdd87dc86d6694cc80c80423905
Mark <m@rkw.me>
2013-12-12 09:54:04 +0000

    [OE-3856] consolidated migrations

--- migrations/m131204_162227_table_versioning.php
+++ migrations/m131204_162227_table_versioning.php
@@ -6,24 +6,24 @@ class m131204_162227_table_versioning extends CDbMigration
 	{
 		$this->execute("
 CREATE TABLE `et_ophdrprescription_details_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `comments` text COLLATE utf8_bin DEFAULT NULL,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `printed` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `locked` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `draft` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `print` tinyint(1) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophdrprescription_details_event_id_fk` (`event_id`),
-  KEY `acv_et_ophdrprescription_details_created_user_id_fk` (`created_user_id`),
-  KEY `acv_et_ophdrprescription_details_last_modified_user_id_fk` (`last_modified_user_id`),
-  CONSTRAINT `acv_et_ophdrprescription_details_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophdrprescription_details_event_id_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
-  CONSTRAINT `acv_et_ophdrprescription_details_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`comments` text COLLATE utf8_bin DEFAULT NULL,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`printed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`locked` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`draft` tinyint(1) unsigned NOT NULL DEFAULT '1',
+	`print` tinyint(1) unsigned NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophdrprescription_details_event_id_fk` (`event_id`),
+	KEY `acv_et_ophdrprescription_details_created_user_id_fk` (`created_user_id`),
+	KEY `acv_et_ophdrprescription_details_last_modified_user_id_fk` (`last_modified_user_id`),
+	CONSTRAINT `acv_et_ophdrprescription_details_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophdrprescription_details_event_id_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
+	CONSTRAINT `acv_et_ophdrprescription_details_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -41,35 +41,35 @@ CREATE TABLE `et_ophdrprescription_details_version` (
 
 		$this->execute("
 CREATE TABLE `ophdrprescription_item_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `prescription_id` int(10) unsigned NOT NULL,
-  `drug_id` int(10) unsigned NOT NULL,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `dose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `route_id` int(10) unsigned NOT NULL,
-  `frequency_id` int(10) unsigned NOT NULL,
-  `duration_id` int(10) unsigned NOT NULL,
-  `route_option_id` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `acv_ophdrprescription_details_prescription_id_fk` (`prescription_id`),
-  KEY `acv_ophdrprescription_details_drug_id_fk` (`drug_id`),
-  KEY `acv_ophdrprescription_details_created_user_id_fk` (`created_user_id`),
-  KEY `acv_ophdrprescription_details_last_modified_user_id_fk` (`last_modified_user_id`),
-  KEY `acv_ophdrprescription_item_route_option_id_fk` (`route_option_id`),
-  KEY `acv_ophdrprescription_item_route_id_fk` (`route_id`),
-  KEY `acv_ophdrprescription_item_frequency_id_fk` (`frequency_id`),
-  KEY `acv_ophdrprescription_item_duration_id_fk` (`duration_id`),
-  CONSTRAINT `acv_ophdrprescription_item_duration_id_fk` FOREIGN KEY (`duration_id`) REFERENCES `drug_duration` (`id`),
-  CONSTRAINT `acv_ophdrprescription_details_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_ophdrprescription_details_drug_id_fk` FOREIGN KEY (`drug_id`) REFERENCES `drug` (`id`),
-  CONSTRAINT `acv_ophdrprescription_details_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_ophdrprescription_details_prescription_id_fk` FOREIGN KEY (`prescription_id`) REFERENCES `et_ophdrprescription_details` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_frequency_id_fk` FOREIGN KEY (`frequency_id`) REFERENCES `drug_frequency` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_route_id_fk` FOREIGN KEY (`route_id`) REFERENCES `drug_route` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_route_option_id_fk` FOREIGN KEY (`route_option_id`) REFERENCES `drug_route_option` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`prescription_id` int(10) unsigned NOT NULL,
+	`drug_id` int(10) unsigned NOT NULL,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`dose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+	`route_id` int(10) unsigned NOT NULL,
+	`frequency_id` int(10) unsigned NOT NULL,
+	`duration_id` int(10) unsigned NOT NULL,
+	`route_option_id` int(10) unsigned DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	KEY `acv_ophdrprescription_details_prescription_id_fk` (`prescription_id`),
+	KEY `acv_ophdrprescription_details_drug_id_fk` (`drug_id`),
+	KEY `acv_ophdrprescription_details_created_user_id_fk` (`created_user_id`),
+	KEY `acv_ophdrprescription_details_last_modified_user_id_fk` (`last_modified_user_id`),
+	KEY `acv_ophdrprescription_item_route_option_id_fk` (`route_option_id`),
+	KEY `acv_ophdrprescription_item_route_id_fk` (`route_id`),
+	KEY `acv_ophdrprescription_item_frequency_id_fk` (`frequency_id`),
+	KEY `acv_ophdrprescription_item_duration_id_fk` (`duration_id`),
+	CONSTRAINT `acv_ophdrprescription_item_duration_id_fk` FOREIGN KEY (`duration_id`) REFERENCES `drug_duration` (`id`),
+	CONSTRAINT `acv_ophdrprescription_details_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_ophdrprescription_details_drug_id_fk` FOREIGN KEY (`drug_id`) REFERENCES `drug` (`id`),
+	CONSTRAINT `acv_ophdrprescription_details_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_ophdrprescription_details_prescription_id_fk` FOREIGN KEY (`prescription_id`) REFERENCES `et_ophdrprescription_details` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_frequency_id_fk` FOREIGN KEY (`frequency_id`) REFERENCES `drug_frequency` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_route_id_fk` FOREIGN KEY (`route_id`) REFERENCES `drug_route` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_route_option_id_fk` FOREIGN KEY (`route_option_id`) REFERENCES `drug_route_option` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -87,26 +87,26 @@ CREATE TABLE `ophdrprescription_item_version` (
 
 		$this->execute("
 CREATE TABLE `ophdrprescription_item_taper_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `item_id` int(10) unsigned NOT NULL,
-  `dose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `frequency_id` int(10) unsigned NOT NULL,
-  `duration_id` int(10) unsigned NOT NULL,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_ophdrprescription_item_taper_item_id_fk` (`item_id`),
-  KEY `acv_ophdrprescription_item_taper_frequency_id_fk` (`frequency_id`),
-  KEY `acv_ophdrprescription_item_taper_duration_id_fk` (`duration_id`),
-  KEY `acv_ophdrprescription_item_taper_created_user_id_fk` (`created_user_id`),
-  KEY `acv_ophdrprescription_item_taper_last_modified_user_id_fk` (`last_modified_user_id`),
-  CONSTRAINT `acv_ophdrprescription_item_taper_item_id_fk` FOREIGN KEY (`item_id`) REFERENCES `ophdrprescription_item` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_taper_frequency_id_fk` FOREIGN KEY (`frequency_id`) REFERENCES `drug_frequency` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_taper_duration_id_fk` FOREIGN KEY (`duration_id`) REFERENCES `drug_duration` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_taper_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_ophdrprescription_item_taper_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`item_id` int(10) unsigned NOT NULL,
+	`dose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+	`frequency_id` int(10) unsigned NOT NULL,
+	`duration_id` int(10) unsigned NOT NULL,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_ophdrprescription_item_taper_item_id_fk` (`item_id`),
+	KEY `acv_ophdrprescription_item_taper_frequency_id_fk` (`frequency_id`),
+	KEY `acv_ophdrprescription_item_taper_duration_id_fk` (`duration_id`),
+	KEY `acv_ophdrprescription_item_taper_created_user_id_fk` (`created_user_id`),
+	KEY `acv_ophdrprescription_item_taper_last_modified_user_id_fk` (`last_modified_user_id`),
+	CONSTRAINT `acv_ophdrprescription_item_taper_item_id_fk` FOREIGN KEY (`item_id`) REFERENCES `ophdrprescription_item` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_taper_frequency_id_fk` FOREIGN KEY (`frequency_id`) REFERENCES `drug_frequency` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_taper_duration_id_fk` FOREIGN KEY (`duration_id`) REFERENCES `drug_duration` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_taper_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_ophdrprescription_item_taper_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -121,10 +121,26 @@ CREATE TABLE `ophdrprescription_item_taper_version` (
 		$this->addColumn('ophdrprescription_item_taper_version','version_id','int(10) unsigned NOT NULL');
 		$this->addPrimaryKey('version_id','ophdrprescription_item_taper_version','version_id');
 		$this->alterColumn('ophdrprescription_item_taper_version','version_id','int(10) unsigned NOT NULL AUTO_INCREMENT');
+
+		$this->addColumn('et_ophdrprescription_details','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophdrprescription_details_version','deleted','tinyint(1) unsigned not null');
+
+		$this->addColumn('ophdrprescription_item','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('ophdrprescription_item_version','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('ophdrprescription_item_taper','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('ophdrprescription_item_taper_version','deleted','tinyint(1) unsigned not null');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('ophdrprescription_item','deleted');
+		$this->dropColumn('ophdrprescription_item_version','deleted');
+		$this->dropColumn('ophdrprescription_item_taper','deleted');
+		$this->dropColumn('ophdrprescription_item_taper_version','deleted');
+
+		$this->dropColumn('et_ophdrprescription_details','deleted');
+		$this->dropColumn('et_ophdrprescription_details_version','deleted');
+
 		$this->dropTable('et_ophdrprescription_details_version');
 		$this->dropTable('ophdrprescription_item_version');
 		$this->dropTable('ophdrprescription_item_taper_version');
