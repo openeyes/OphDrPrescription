2a3375186cc1c35cfdf2b58b396a99c4d5169751
Mark <m@rkw.me>
2014-03-03 15:47:34 +0000

    [OE-3987] transactions

---
+++ migrations/m140303_154550_transactions.php
@@ -0,0 +1,42 @@
+<?php
+
+class m140303_154550_transactions extends CDbMigration
+{
+	public $tables = array('et_ophdrprescription_details','ophdrprescription_item_taper','ophdrprescription_item');
+
+	public function up()
+	{
+		foreach ($this->tables as $table) {
+			$this->addColumn($table,'hash','varchar(40) not null');
+			$this->addColumn($table,'transaction_id','int(10) unsigned null');
+			$this->createIndex($table.'_tid',$table,'transaction_id');
+			$this->addForeignKey($table.'_tid',$table,'transaction_id','transaction','id');
+
+			$this->addColumn($table.'_version','hash','varchar(40) not null');
+			$this->addColumn($table.'_version','transaction_id','int(10) unsigned null');
+			$this->addColumn($table.'_version','deleted_transaction_id','int(10) unsigned null');
+			$this->createIndex($table.'_vtid',$table.'_version','transaction_id');
+			$this->addForeignKey($table.'_vtid',$table.'_version','transaction_id','transaction','id');
+			$this->createIndex($table.'_dtid',$table.'_version','deleted_transaction_id');
+			$this->addForeignKey($table.'_dtid',$table.'_version','deleted_transaction_id','transaction','id');
+		}
+	}
+
+	public function down()
+	{
+		foreach ($this->tables as $table) {
+			$this->dropColumn($table,'hash');
+			$this->dropForeignKey($table.'_tid',$table);
+			$this->dropIndex($table.'_tid',$table);
+			$this->dropColumn($table,'transaction_id');
+
+			$this->dropColumn($table.'_version','hash');
+			$this->dropForeignKey($table.'_vtid',$table.'_version');
+			$this->dropIndex($table.'_vtid',$table.'_version');
+			$this->dropColumn($table.'_version','transaction_id');
+			$this->dropForeignKey($table.'_dtid',$table.'_version');
+			$this->dropIndex($table.'_dtid',$table.'_version');
+			$this->dropColumn($table.'_version','deleted_transaction_id');
+		}
+	}
+}
