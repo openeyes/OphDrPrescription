5da8176e04ed9f23e4708fababc2ad589cb1f3e3
pgwanz <pgwanz@hotmail.com>
2015-10-15 13:53:27 +0100

    avoid popups when page is already loaded and in edit/create state

--- assets/js/defaultprescription.js
+++ assets/js/defaultprescription.js
@@ -263,6 +263,9 @@ function getNextKey() {
 $(document).ready(function() {
     if(window.location.href.indexOf("/update/") > -1) return;
 
+    var error_count = $('div.errorlink').length;
+
+
     var today = $.datepicker.formatDate('dd-M-yy', new Date());
     var show_warning = false;
     var ol_list = $("body").find("ol.events");
@@ -296,7 +299,7 @@ $(document).ready(function() {
         })
     })
 
-    if(show_warning)
+    if(show_warning && error_count > 0)
     {
         var warning_message = 'Prescriptions have already been created for this patient today.';
         if(prescription_count == 1)
@@ -342,3 +345,5 @@ function goBack()
 {
     window.history.back();
 }
+
+
